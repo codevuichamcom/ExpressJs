4a35049257d692f342e30643e7ff2eb7f890196a
lehongquan15a <lehongquan15a@gmail.com>
2020-05-29 19:36:15 +0700

    authentication

---
+++ Bai15 Authentication/app.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var app = express();
+var shortid = require('shortid');
+
+var cookieParser = require('cookie-parser');
+
+var userRoute = require('./routes/user.route');
+var authRoute = require('./routes/auth.route');
+
+var authMiddleware = require('./middlewares/auth.middleware');
+
+
+var port = 3000;
+
+
+
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+app.use(cookieParser());
+
+app.use(express.static('public'))
+
+app.get('/',function(res, res){
+    res.render('index');
+});
+
+app.use('/users',authMiddleware.requireAuth,userRoute);
+app.use('/auth',authRoute);
+app.listen(port, function(){
+console.log('Server start at '+port);
+});
---
+++ Bai15 Authentication/controller/auth.controller.js
@@ -0,0 +1,31 @@
+var db = require("../db");
+
+module.exports.login = function (req, res) {
+    res.render("auth/login");
+};
+
+module.exports.postlogin = function (req, res) {
+    var email = req.body.email;
+    var password = req.body.password;
+    var user = db.get('users').find({email:email}).value();
+
+    if(!user){
+        res.render('auth/login',{
+            errors:[
+                'User does not exist.'
+            ]
+        });
+        return;
+    }
+
+    if(user.password !== password){
+        res.render('auth/login',{
+            errors:[
+                'Wrong password.'
+            ]
+        });
+        return;
+    }
+    res.cookie('userId',user.id);
+    res.redirect('/users');
+};
